59a637fca8592d9f41067f3c4868594b07079ada
Robert Thau <rst@ai.mit.edu>
2012-05-10 15:09:00 -0400

    Call options screen displays call options correctly.

--- src/main/scala/CallOptionsActivity.scala
+++ src/main/scala/CallOptionsActivity.scala
@@ -1,6 +1,25 @@
 package org.positronicnet.sample.contacts
 
 import org.positronicnet.ui._
+import android.media.RingtoneManager
+import android.net.Uri
 
 class CallOptionsActivity 
   extends PositronicActivity( layoutResourceId = R.layout.call_options )
+  with TypedViewHolder
+{
+  onCreate {
+    val contactSlug = getIntent.getSerializableExtra( "contact" )
+    val contact = contactSlug.asInstanceOf[ Contact ]
+    ContactsActivityUiBinder.show( contact, findView( TR.call_options ))
+    updateRingtone( contact.customRingtone )
+  }
+
+  def updateRingtone( ringtoneUriStr: String ) = 
+    findView( TR.ringtoneLabel ).setText(
+      if (ringtoneUriStr == null || ringtoneUriStr == "")
+        "(default)"
+      else
+        RingtoneManager.getRingtone( this, Uri.parse( ringtoneUriStr )).getTitle( this )
+    )
+}
