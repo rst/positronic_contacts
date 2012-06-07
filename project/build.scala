2a620f350cb21a5fc8efc67b502402db5f978b62
Robert Thau <rst@ai.mit.edu>
2012-06-07 19:54:58 -0400

    Add dependencies required to actually run the tests.

--- project/build.scala
+++ project/build.scala
@@ -18,8 +18,13 @@ object General {
       keyalias in Android := "change-me",
       libraryDependencies ++= Seq(
         "org.scalatest"     %% "scalatest"        % "1.6.1"        % "test",
+        "org.positronicnet" %% "roboscalatest"    % "0.4-SNAPSHOT" % "test",
+        "com.pivotallabs"   %  "robolectric"      % "1.0-RC1"      % "test",
         "org.positronicnet" %% "positronicnetlib" % "0.4-SNAPSHOT"
       ),
+      testOptions in Test ++= Seq(
+        Tests.Argument("-DandroidResPath=src/main/res"),
+        Tests.Argument("-DandroidManifestPath=src/main/AndroidManifest.xml")),
       proguardOption in Android := """
        -keepclassmembers class * implements java.io.Serializable {
         private static final java.io.ObjectStreamField[] serialPersistentFields;
